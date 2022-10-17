9a82798fbaf6cf6335575d8ee7e11a1a616aafec
InesMansouri8 <115554512+InesMansouri8@users.noreply.github.com>
2022-10-17 17:43:56 +0200

    Add files via upload

---
+++ Cv_Main-main/cours/cours.js
@@ -0,0 +1,5 @@
+let btn1 = document.querySelector('#btn1');
+let btn2 = document.querySelector('#btn2');
+
+let clickBtn1 = btn1.addEventListener('click', () =>{console.log("oui")});
+let clickBtn2 = btn2.addEventListener('click', () => {console.log("non")});
